77916b68a48fcf3ec9a503289a887359c260dd0d
cashicoawe <cashico.awe@gmail.com>
2025-02-10 02:20:41 -0800

    fix isVailedList

--- pkg/Lisp.java
+++ pkg/Lisp.java
@@ -17,23 +17,20 @@ public class Lisp {
         // 括弧がすべて対応する
         // 括弧で囲まれている = 対応数が、終了以外で 0 にならない
         String[] ss = s.split(" ");
-        Debug.p("isVailedList", "ss", Arrays.toString(ss));
-        Debug.p("isVailedList", "ss_length", Integer.toString(ss.length));
         if(ss.length == 1){
             return !ss[0].equals("(") && !ss[0].equals(")");
         }
-        // if(ss[0] != "(") return false;
-        // if(ss[ss.length - 1] != ")") return false;
         int prth = 0;
-        for(int i = 0; i < ss.length - 1; i++){
-            if (ss[i].equals("(")) {prth = prth + 1;}
+        for(int i = 0; i < ss.length; i++){
+            if (ss[i].equals("(")) prth = prth + 1;
             if (ss[i].equals(")")) prth = prth - 1;
-            // Debug.p("isVailedList", "prth", (ss[i] == "("));
             if (i != ss.length - 1 && prth == 0){
+                Debug.p("isVailedList", "close-prth", prth);
                 return false;
             }
         }
-        return true;
+        Debug.p("isVailedList", "open-prth", prth);
+        return prth == 0;
     }
     static String toSExpression(String s){
         return "";
